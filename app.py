f70f328a328f2345ab6bfd5849f7a43a60de2027
Manisha K <143818963+manisha-024@users.noreply.github.com>
2024-02-18 23:03:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,26 +12,34 @@ def get_gemini_response(question):
     response = chat.send_message(question, stream=True)
     return response 
 
-st.set_page_config(page_title="Q&A Demo")
+st.set_page_config(page_title="BioBot")
 
 st.header("BioBot")
 
 if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = []
+you_style = "color: #aaccff;"
+biobot_style = "color:white;"
 
-st.subheader("History")
+# Display chat history
+st.subheader("History:")
 
 for role, text in st.session_state['chat_history']:
-    st.write(f"{role}: {text}")
+    if role == "You":
+        st.markdown(f'<p style="{you_style}">You: {text}</p>', unsafe_allow_html=True)
+    else:
+        st.markdown(f'<p style="{biobot_style}">BioBot: {text}</p>', unsafe_allow_html=True)
 
 input_text = st.text_input("Your message:", key="input", value="")
 submit_button = st.button("Ask")
-
 if submit_button and input_text:
     st.session_state['chat_history'].append(("You", input_text))
     response = get_gemini_response(input_text)
-    for chunk in response:
-        st.session_state['chat_history'].append(("BioBot", chunk.text))
+    
+    # Concatenate all response chunks into a single string
+    response_text = ' '.join([chunk.text for chunk in response])
+    
+    st.session_state['chat_history'].append(("BioBot", response_text))
     
     if 'input_text' in st.query_params:
         del st.query_params['input_text']
